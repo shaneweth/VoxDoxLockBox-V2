7aa36bac2c4680f9f54d35c13a4c9aa557fb75e2
willow <shaneweth77@gmail.com>
2019-07-08 14:28:04 -0400

    first components

---
+++ vox-dox-v2/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './components/Header';
+import ReactDOM from 'react-dom';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+  return (
+    <div className="grid-container">
+      <Header />
+    </div>
+  )
+}
+}
+export default App;

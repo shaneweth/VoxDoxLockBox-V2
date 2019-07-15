9a2e6be81aafc5c276abe0dfff234fa7827ed46f
unknown <mstevens9335@yahoo.com>
2019-07-15 17:20:07 -0400

    add s3 stuff

--- vox-dox-v2/backend/server.js
+++ vox-dox-v2/backend/server.js
@@ -1,3 +1,4 @@
+// dependencies
 const express = require('express')
 const app = express();
 const bodyParser = require('body-parser');
@@ -5,21 +6,47 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const projRoutes = express.Router();
 const PORT = 2112;
+const fileUpload = require("express-fileupload");
+var multer = require('multer')
+const aws = require('aws-sdk');
+const dotenv = require('dotenv');
 
 let Proj = require('./proj.model');
 
+// middleware
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(fileUpload());
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'files')
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname)
+    }
+})
+
+// amazon s3 config
+dotenv.config();
+aws.config.update({
+ 
+});
+
+// multer
+var upload = multer({ storage: storage }).single('file');
+
 
 mongoose.connect('mongodb://localhost:27017/projects', { useNewUrlParser: true });
 const connection = mongoose.connection;
 
-connection.once('open', function() {
+connection.once('open', function () {
     console.log("MONGO PLUG IN REAL GOOD")
 })
 
-projRoutes.route('/').get(function(req, res) {
-    Proj.find(function(err, projs) {
+projRoutes.route('/').get(function (req, res) {
+    Proj.find(function (err, projs) {
         if (err) {
             console.log(err);
         } else {
@@ -28,26 +55,57 @@ projRoutes.route('/').get(function(req, res) {
     });
 });
 
-projRoutes.route('/add').post(function(req, res) {
+projRoutes.route('/add').post(function (req, res) {
+    // amazon s3
+    const s3 = new aws.S3();
+    upload(req, res, function (err) {
+
+        //    req.file is the file itself
+        console.log(req.files.file.data);
+        // get file
+        let file = req.files.file.data;
+        // get file name
+        let fName = req.files.file.name;
+
+        // bucket parameters
+        let params = {
+            Bucket: 'voxdox',
+            ACL: 'public-read',
+            Body: file,
+            Key: fName,
+        };
+
+        // s3 upload function
+        s3.upload(params, function (err, data) {
+            if (err) {
+                console.log("Error", err);
+            } if (data) {
+                console.log("Upload Success", data.Location);
+            }
+        });
+    })
+
+
+    // mongoDB
     let proj = new Proj(req.body);
     proj.save()
         .then(proj => {
-            res.status(200).json({'project': 'added successfully'});
+            res.status(200).json({ 'project': 'added successfully' });
         })
         .catch(err => {
             res.status(400).send('adding new failed');
         })
 })
 
-projRoutes.route('/edit/:id').get(function(req, res) {
+projRoutes.route('/edit/:id').get(function (req, res) {
     let id = req.params.id;
-    Proj.findById(id, function(err, response) {
+    Proj.findById(id, function (err, response) {
         res.json(response);
     })
 })
 
 app.use('/projs', projRoutes);
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log("connected to port: " + PORT);
 })
---
+++ vox-dox-v2/routes/apiRoutes.js
@@ -0,0 +1,121 @@
+// dotenv.config();
+// aws.config.update({
+//     accessKeyId: "AKIATXD362LLGCFKOXXK",
+//     secretAccessKey: "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"
+// });
+
+require("express-fileupload");
+const db = require("../models");
+const aws = require('aws-sdk');
+const dotenv = require('dotenv');
+
+
+
+dotenv.config();
+aws.config.update({
+    accessKeyId: "AKIATXD362LLGCFKOXXK",
+    secretAccessKey: "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"
+});
+
+
+
+module.exports = function (app) {
+    app.get("/api/projects", function (req, res) {
+        let query = {};
+        if (req.query.oName) {
+            query.oName = req.query.oName;
+        }
+
+        db.Project.findAll({
+            where: query,
+            include: [db.User],
+        }).then(function (dbProject) {
+            res.json(dbProject);
+        });
+    });
+
+
+
+    app.get("/index", function (req, res) {
+
+    });
+
+    app.get("/", function (req, res) {
+        console.log("console data");
+        db.Project.findAll({ attributes: ['title', 'url'] }).then(function (projects) {
+
+            //  db data consoled
+            // console.log(projects['title']);
+            var hbsObject = { files: projects };
+            for (var i = 0; i < projects.length; i++) {
+                // console.log(projects[i]['url']);
+                var url = projects[i]['url'];
+
+
+            }
+
+
+            res.render("index", hbsObject);
+
+        });
+    });
+
+
+
+    app.post("/api/projects", function (req, res) {
+        const s3 = new aws.S3();
+
+        console.log(req.files[0]);
+
+        let file = req.files[0];
+        let buffer = file.buffer;
+
+        let params = {
+            Bucket: 'teamawesome55',
+            ACL: 'public-read',
+            Body: buffer,
+            Key: req.body.description + "/" + req.body.title + "/" + file.originalname,
+        }
+
+        s3.upload(params, function (err, data) {
+            if (err) throw err;
+            if (data) {
+                console.log(data);
+                let newProject = {
+                    title: req.body.title,
+                    description: req.body.description,
+                    url: data.Location,
+                }
+
+
+
+                db.Project.create(newProject).then(function (dbProject) {
+                    res.json(dbProject);
+                });
+                res.redirect('/');
+            }
+        });
+    });
+
+    app.delete("/api/projects/:title", function (req, res) {
+        db.Project.destroy({
+            where: {
+                title: req.params.title
+            }
+        }).then(function (dbProject) {
+            res.json(dbProject);
+        });
+    });
+
+    app.put("/api/projects", function (req, res) {
+        db.Project.update(
+            req.body,
+            {
+                where: {
+                    title: req.body.title
+                }
+            }).then(function (dbProject) {
+                res.json(dbProject);
+            });
+    });
+}

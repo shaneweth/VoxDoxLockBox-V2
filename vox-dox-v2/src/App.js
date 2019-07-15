f1d25c63aefd31218c5845901c2b6e353b8ba26b
willow <shaneweth77@gmail.com>
2019-07-15 00:14:53 -0400

    axios post route is working with the form. need to connect s3 methods

--- vox-dox-v2/src/App.js
+++ vox-dox-v2/src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import Header from './components/Header';
-import TapeDeck from './components/TapeDeck/TapeDeck';
+// import TapeDeck from './components/TapeDeck/TapeDeck';
 import './App.css';
 
 class App extends React.Component {
   render() {
-  return (
-    <div className="grid-container">
-      <Header />
-      {/* <TapeDeck /> */}
-    </div>
-  )
-}
+    return (
+      <div className="grid-container">
+        <Header />
+        {/* <TapeDeck /> */}
+      </div>
+    )
+  }
 }
 export default App;
--- vox-dox-v2/src/components/Form/Form.js
+++ vox-dox-v2/src/components/Form/Form.js
@@ -1,40 +1,107 @@
 import React from 'react';
+import axios from 'axios';
 import "./Form.css";
 
 class Form extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.onChangeProjTitle = this.onChangeProjTitle.bind(this);
+    this.onChangeProjDescription = this.onChangeProjDescription.bind(this);
+    this.onChangeProjUrl = this.onChangeProjUrl.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      proj_title: '',
+      proj_description: '',
+      proj_url: ''
+    }
+  }
+
+  // methods
+
+  onChangeProjTitle(e) {
+    this.setState({
+      proj_title: e.target.value
+    });
+  }
+
+  onChangeProjDescription(e) {
+    this.setState({
+      proj_description: e.target.value
+    });
+  }
+
+  onChangeProjUrl(e) {
+    this.setState({
+      proj_url: e.target.value
+    })
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    console.log('form submitted');
+    console.log(`Title: ${this.state.proj_title}`);
+    console.log(`Description: ${this.state.proj_description}`);
+    console.log(`URL: ${this.state.proj_url}`);
+
+    const newProj = {
+      proj_title: this.state.proj_title,
+      proj_description: this.state.proj_description,
+      proj_url: this.state.proj_url
+    };
+
+    axios.post('http://localhost:2112/projs/add', newProj)
+        .then(res => console.log(res.data));
+
+        this.setState({
+          proj_title: '',
+          proj_description: '',
+          proj_url: '',
+        })
+  }
+
   render() {
     return (
-        <div className="uploadForm">
-            <form action="/api/projects" enctype="multipart/form-data" method="POST">
-                <div className="row">
-                <h2>Upload Form</h2>
-                </div>
+      <div className="uploadForm">
+        <form onSubmit={this.onSubmit} >
+          <div className="row">
+            <h2>Upload Form</h2>
+          </div>
 
-        <div className="row">
-            <label for="title">
-            <h3>title</h3>
+          <div className="row">
+            <label>
+              <h3>title</h3>
             </label>
-            <input type="text" name="title" />
-        </div>
+            <input    type="text" 
+                      name="title"
+                      value={this.state.proj_title}
+                      onChange={this.onChangeProjTitle} 
+                      />
+          </div>
+          <div className="row">
+            <label>
+              <h3>description</h3>
+            </label>
+            <input    type="text" 
+                      name="description" 
+                      value={this.state.proj_description}
+                      onChange={this.onChangeProjDescription} 
+                      />
 
-
-      <div className="row">
-        <label for="description">
-          <h3>description</h3>
-        </label>
-        <input type="text" name="description" />
-
+          </div>
+          <hr />
+          <div className="row" id="fileUpload">
+            <br />
+            <input type="file" name="myFile" className="fileInput" />
+            <button className="submitBtn" type="submit" value="Create Project">Create</button>
+          </div>
+        </form>
       </div>
-      <hr />
-      <div className="row" id="fileUpload">
-        <br />
-        <input type="file" name="myFile" className="fileInput" />
-        <button className="submitBtn" type="submit" value="send">send</button>
-      </div>
-    </form>
-  </div>
-  )
-}
+    )
+  }
 }
 
 export default Form;

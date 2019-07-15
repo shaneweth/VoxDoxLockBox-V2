624526a922f15ef24c1f0d570f14806c8de89943
willow <shaneweth77@gmail.com>
2019-07-15 11:51:01 -0400

    front and back connected. no edit route yet.

--- vox-dox-v2/src/App.js
+++ vox-dox-v2/src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Header from './components/Header';
 // import TapeDeck from './components/TapeDeck/TapeDeck';
 import './App.css';
+import Present from './components/Present';
 
 class App extends React.Component {
   render() {
     return (
+      <Router>
       <div className="grid-container">
         <Header />
         {/* <TapeDeck /> */}
       </div>
+
+        <Route path="/" exact component={Present}></Route>
+        {/* <Route path="/edit/:id" exact component={EditProj} /> */}
+      </Router>
     )
   }
 }
--- vox-dox-v2/src/components/Present/Present.js
+++ vox-dox-v2/src/components/Present/Present.js
@@ -1,17 +1,62 @@
 import React from 'react';
 import "./Present.css";
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+const Proj = props => (
+  <tr>
+    <td>{props.proj.proj_title}</td>
+    <td>{props.proj.proj_description}</td>
+    <td>{props.proj.proj_url}</td>
+    <td>
+      <Link to={'/edit/' + props.proj._id}>EDIT</Link>
+    </td>
+  </tr>
+)
 
 class Present extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {projs: []};
+
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:2112/projs/')
+        .then(response => {
+          this.setState({projs: response.data});
+        })
+        .catch(function (err) {
+          console.log(err);
+        })
+  }
+
+  projList() {
+    return this.state.projs.map(function(currentProj, i) {
+      return <Proj proj={currentProj} key={i} />
+    })
+  }
+
   render() {
-  return (
-    <div className="projView">
-        <ul>
-          <li>{"project"}</li>
-          <button className="btn-select" id="btnUrl" type="submit">Select</button>
-        </ul>
-    </div>
-  )
-}
+    return (
+      <div className="projView">
+        <h3>Projects</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Notes</th>
+              <th>Link</th>
+            </tr>
+          </thead>
+          <tbody>
+            { this.projList() }
+          </tbody>
+        </table>
+      </div>
+    )
+  }
 }
 
   export default Present;
